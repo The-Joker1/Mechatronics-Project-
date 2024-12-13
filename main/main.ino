008a0f1b033c0b563e69e9cc76d77f18397a8e4f
LoricLannister <marcellusaba@gmail.com>
2024-12-13 16:32:44 +0100

    encoders done

--- main/main.ino
+++ main/main.ino
@@ -6,104 +6,71 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1);
 Adafruit_DCMotor *myMotor2 = AFMS.getMotor(2);
 
-#define CPT_LEFT 3
-#define CPT_RIGHT 2
+// Définitions des broches des encodeurs
+#define CPT_LEFT 3   // Broche de l'encodeur du moteur gauche
+#define CPT_RIGHT 2  // Broche de l'encodeur du moteur droit
 
-#define CPT_US_TRIG_PIN 10
-#define CPT_US_ECHO_PIN 11
+#define SPEED 50  // Vitesse des moteurs (0 à 255)
+#define TICKS_PER_CM 2  // 330/(pi*5.8)
+#define DISTANCE_CM 15  // Distance cible en cm
 
-#define SPEED 50
+// Variables pour compter les ticks
+volatile int ticksLeft = 0;
+volatile int ticksRight = 0;
 
-// Variable pour la distance
-float distance = 0.0;
-// Distance seuil en cm
-const float seuil = 20.0;
-// Variable booléenne
-bool estProche = true;
+// Fonction appelée lors d'une interruption pour compter les ticks du moteur gauche
+void countLeft() {
+  ticksLeft++;
+}
+
+// Fonction appelée lors d'une interruption pour compter les ticks du moteur droit
+void countRight() {
+  ticksRight++;
+}
 
 void setup() {
-  // Démarrer la communication série
+  // Initialisation de la communication série
   Serial.begin(9600);
-  while (!Serial); // Attendre que la connexion série soit établie
-
-  // Log que le programme a démarré
-  Serial.println("Programme démarré!");
-
-  pinMode(CPT_LEFT, INPUT);
-  pinMode(CPT_RIGHT, INPUT);
-
-  pinMode(CPT_US_TRIG_PIN, OUTPUT);
-  pinMode(CPT_US_ECHO_PIN, INPUT);
+  Serial.println("Initialisation...");
 
   // Initialisation du shield moteur
   AFMS.begin();
-  delay(5000);  // Attendre l'initialisation du shield moteur
 
-  // Log que le shield moteur est initialisé
-  Serial.println("Shield moteur initialisé.");
+  // Configuration des broches d'encodeur
+  pinMode(CPT_LEFT, INPUT_PULLUP);
+  pinMode(CPT_RIGHT, INPUT_PULLUP);
+
+  // Attachement des interruptions pour les encodeurs
+  attachInterrupt(digitalPinToInterrupt(CPT_LEFT), countLeft, RISING);
+  attachInterrupt(digitalPinToInterrupt(CPT_RIGHT), countRight, RISING);
+
+  // Démarrage des moteurs
+  myMotor1->setSpeed(SPEED);
+  myMotor2->setSpeed(SPEED);
+  myMotor1->run(FORWARD);
+  myMotor2->run(FORWARD);
 }
 
 void loop() {
-  // Lire les valeurs des capteurs
-  bool left = digitalRead(CPT_LEFT);  // Détection du noir
-  bool right = digitalRead(CPT_RIGHT); // Détection du noir
+  // Calcul du nombre total de ticks requis pour atteindre la distance cible
+  int targetTicks = DISTANCE_CM * TICKS_PER_CM;
 
-  // Log des valeurs détectées par les capteurs
-  Serial.print("CPT_LEFT: ");
-  Serial.print(left);
-  Serial.print("  CPT_RIGHT: ");
-  Serial.println(right);
+  // Affiche les ticks actuels (pour le débogage)
+  Serial.print("Ticks Left: ");
+  Serial.print(ticksLeft);
+  Serial.print(" | Ticks Right: ");
+  Serial.println(ticksRight);
 
-   // Génération de l'impulsion sur le Trig
-  digitalWrite(CPT_US_TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(CPT_US_TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(CPT_US_TRIG_PIN, LOW);
-
-  // Lecture de l'impulsion sur l'Echo
-  long duration = pulseIn(CPT_US_ECHO_PIN, HIGH);
-
-  // Calcul de la distance en cm
-  distance = (duration * 0.034) / 2;
-
-  // Mise à jour de la variable booléenne
-  if (distance > seuil) {
-    estProche = false;
-  } else {
-    estProche = true;
+  // Vérifie si les deux moteurs ont atteint la distance cible
+  if (ticksLeft >= targetTicks && ticksRight >= targetTicks) {
+    stopMotors(); // Arrête les moteurs
+    Serial.println("Distance atteinte !");
+    while (1);    // Arrêt du programme
   }
+}
 
-  if(!estProche){
-    Serial.println("NO OBSTACLE");
-    if (left && right) {
-      Serial.println("FORWARD");
-      myMotor1->setSpeed(SPEED);
-      myMotor2->setSpeed(SPEED);
-      myMotor1->run(FORWARD);
-      myMotor2->run(FORWARD);
-    } else if (left && !right) {
-      Serial.println("LEFT");
-      myMotor1->setSpeed(SPEED);
-      myMotor1->run(RELEASE);
-      myMotor2->run(FORWARD);
-    } else if (!left && right) {
-      Serial.println("RIGHT");
-      myMotor2->setSpeed(SPEED);
-      myMotor1->run(FORWARD);
-      myMotor2->run(RELEASE);
-    } else {
-      Serial.println("STOP");
-      myMotor1->run(RELEASE);
-      myMotor2->run(RELEASE);
-    }
-  } else {
-    Serial.println("OBSTACLE");
-    Serial.println("STOP");
-    myMotor1->run(RELEASE);
-    myMotor2->run(RELEASE);
-  }
-
- // delay(1000);
-
+// Fonction pour arrêter les moteurs
+void stopMotors() {
+  myMotor1->run(RELEASE);
+  myMotor2->run(RELEASE);
 }
